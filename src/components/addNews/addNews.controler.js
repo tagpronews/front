3a00910d382d26c9dde2f04ff2708ae9d4ba90e3
Oliver Shanklin <odogollie@gmail.com>
2015-02-19 16:55:56 -0700

    v1 of the addNews component
    Need to add some references to the module i used for the tables, info
    can be found here, http://bazalt-cms.com/ng-table/

---
+++ src/components/addNews/addNews.controler.js
@@ -0,0 +1,21 @@
+var app = angular.module('main', ['ngTable']).
+
+controller('TableCtrl', function($scope) {
+
+
+
+    $scope.articles = [
+
+            {headline: "This is a headline. And it is amazing!", category: "Leagues", date: "2 Feb 2015", author: "odog"},
+
+            {headline: "Hopefully this will be a two line headline, but who knows.", category: "News", date: "25 Jan 2015", author: "odog"},
+
+            {headline: "MLTP is disbanding!", category: "Videos", date: "30 Feb 2015", author: "odog"},
+
+            {headline: "Streaming tips and tricks", category: "Mumble", date: "16 Mar 2015", author: "odog"},
+
+            {headline: "Some other headline becuase I can't think of anything.", category: "General", date: "1 Feb 2015", author: "odog"}
+
+        ];
+
+})
